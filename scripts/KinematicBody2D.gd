04d23a2f5fb5b84df08d6d3a1e04d30fb9324fa7
Jonathan Moallem <jdmoallem@gmail.com>
2019-09-15 16:31:49 +1000

    Changed movement updates to run during the physics processing

--- scripts/KinematicBody2D.gd
+++ scripts/KinematicBody2D.gd
@@ -1,6 +1,5 @@
 extends KinematicBody2D
 
-
 export (int) var speed = 200
 var velocity = Vector2()
 
@@ -8,8 +7,7 @@ var velocity = Vector2()
 func _ready():
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	velocity = Vector2()
 	if Input.is_action_pressed('right'):
 		velocity.x += 1
