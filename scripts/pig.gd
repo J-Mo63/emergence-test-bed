ac44f1a90a5608b62d1d1dd93cf7b569677c0681
Jonathan Moallem <jdmoallem@gmail.com>
2019-09-16 19:53:25 +1000

    Made pig move towards kinematic bodies within line of sight

--- scripts/pig.gd
+++ scripts/pig.gd
@@ -6,10 +6,20 @@ var target = Vector2()
 var velocity = Vector2()
 
 onready var anim = $AnimationPlayer
+onready var line = $"LineOfSight"
+
+class_name Pig
 
 func _input(event):
 	if event.is_action_pressed('click'):
 		target = get_global_mouse_position()
+
+func _process(_delta):
+	var bodies = line.get_overlapping_bodies()
+	
+	for body in bodies:
+		if body.get_instance_id () != get_instance_id ():
+			target = body.position
 
 func _physics_process(_delta):
 	velocity = (target - position).normalized() * speed
