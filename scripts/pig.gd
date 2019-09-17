a7bb11d4b52f74a0c14e4cad1671ff8a898e9941
Jonathan Moallem <jdmoallem@gmail.com>
2019-09-17 14:01:44 +1000

    Made pig "sniff" out any nearby entities in the test scene

--- scripts/pig.gd
+++ scripts/pig.gd
@@ -8,18 +8,13 @@ var velocity = Vector2()
 onready var anim = $AnimationPlayer
 onready var line = $"LineOfSight"
 
+var checked = [get_instance_id()]
+
 class_name Pig
 
 func _input(event):
 	if event.is_action_pressed('click'):
 		target = get_global_mouse_position()
-
-func _process(_delta):
-	var bodies = line.get_overlapping_bodies()
-	
-	for body in bodies:
-		if body.get_instance_id () != get_instance_id () and body.is_in_group("Entity"):
-			target = body.position
 
 func _physics_process(_delta):
 	velocity = (target - position).normalized() * speed
@@ -27,4 +22,10 @@ func _physics_process(_delta):
 		velocity = move_and_slide(velocity)
 		anim.play("walk_right")
 	else:
-		anim.play("rest")
+		var bodies = line.get_overlapping_bodies()
+		for body in bodies:
+			if !checked.has(body.get_instance_id()) and body.is_in_group("Entity"):
+				target = body.position
+				checked.append(body.get_instance_id())
+				break
+		#anim.play("rest")
