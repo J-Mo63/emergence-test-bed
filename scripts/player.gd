4e46ab197317e174d49f18c321aef6a475efaa0c
Jonathan Moallem <jdmoallem@gmail.com>
2019-09-19 15:34:42 +1000

    Updated player movement script to handle 8-directional animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,19 +5,40 @@ var velocity = Vector2()
 onready var anim = $AnimationPlayer
 
 func _physics_process(_delta):
-	velocity = Vector2()
+	var temp_velocity = Vector2()
 	if Input.is_action_pressed('right'):
-		velocity.x += 1
-		anim.play("walk_right")
+		temp_velocity.x += 1
 	if Input.is_action_pressed('left'):
-		velocity.x -= 1
-		anim.play("walk_left")
+		temp_velocity.x -= 1
 	if Input.is_action_pressed('down'):
-		velocity.y += 1
-		anim.play("walk_down")
+		temp_velocity.y += 1
 	if Input.is_action_pressed('up'):
-		velocity.y -= 1
-		anim.play("walk_up")
-	velocity = velocity.normalized() * speed
-	
+		temp_velocity.y -= 1
+		
+	velocity = temp_velocity.normalized() * speed
 	velocity = move_and_slide(velocity)
+	
+	var play_anim
+	print(str(temp_velocity.x) + ', ' + str(temp_velocity.y))
+	if temp_velocity.x != 0 and temp_velocity.y != 0:
+		if temp_velocity.y == 1 and temp_velocity.x == -1:
+			play_anim = "walk_down_left"
+		if temp_velocity.y == 1 and temp_velocity.x == 1:
+			play_anim = "walk_down_right"
+		if temp_velocity.y == -1 and temp_velocity.x == -1:
+			play_anim = "walk_up_left"
+		if temp_velocity.y == -1 and temp_velocity.x == 1:
+			play_anim = "walk_up_right"
+	elif temp_velocity.x == 0 and temp_velocity.y == 0:
+		play_anim = "rest"
+	else:
+		if temp_velocity.x == -1:
+			play_anim = "walk_left"
+		if temp_velocity.x == 1:
+			play_anim = "walk_right"
+		if temp_velocity.y == 1:
+			play_anim = "walk_down"
+		if temp_velocity.y == -1:
+			play_anim = "walk_up"
+	
+	anim.play(play_anim)
