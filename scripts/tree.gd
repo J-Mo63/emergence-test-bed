f0836073c267e410916fda9f8faf548fce4c3fe3
Jonathan Moallem <jdmoallem@gmail.com>
2019-09-21 14:29:45 +1000

    Added ability for trees to be cut down

--- scripts/tree.gd
+++ scripts/tree.gd
@@ -1,16 +1,31 @@
 extends StaticBody2D
 
-export var offspring_range = 50
+export var offspring_padding = 2
 var tree_scene = load("res://scenes/entities/tree.tscn")
+var health = 50
 
 func _ready():
-	$Timer.connect("timeout", self, "_on_timer_timeout")
+	# $Timer.connect("timeout", self, "_on_timer_timeout")
+	pass
 
 
 func _on_timer_timeout():
 	var tree_instance = tree_scene.instance()
-	var pos_x = rand_range(position.x - offspring_range, position.x + offspring_range)
-	var pos_y = rand_range(position.y - offspring_range, position.y + offspring_range)
+	var tree_width = $Sprite.texture.get_size().x / 6
+	var angle = rand_range(0, 361)
+	var new_dir = Vector2(cos(angle), sin(angle))
 	
-	tree_instance.position = Vector2(pos_x, pos_y)
-	get_node("/root").add_child(tree_instance)
+	tree_instance.position = position + new_dir * tree_width * offspring_padding
+	
+	var space_state = get_world_2d().direct_space_state
+	var result = space_state.intersect_ray(position, tree_instance.position)
+	
+	if !result:
+		get_parent().add_child(tree_instance)
+
+
+func _cut():
+	health -= 1
+	if health == 0:
+		queue_free()
+		return "wood"
