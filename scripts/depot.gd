2c5d5579b13b957f3302cd12f5f926924ca03278
Jonathan Moallem <jdmoallem@gmail.com>
2019-09-25 15:55:10 +1000

    Made depots upgradable

--- scripts/depot.gd
+++ scripts/depot.gd
@@ -1,10 +1,15 @@
 extends Area2D
 
 export var item_scatter = 30
-var items = []
+var items = {}
+var depot_level = 0
+
 
 func _deposit(item):
-	items.append(item)
+	if items.has(item):
+		items[item] = items.get(item) + 1
+	else:
+		items[item] = 1
 	var item_sprite = Sprite.new()
 	var image_texture = ImageTexture.new()
 	image_texture.create_from_image(load("res://assets/sprites/" + item + ".png"))
@@ -12,3 +17,16 @@ func _deposit(item):
 	item_sprite.texture = image_texture
 	item_sprite.position = Vector2(rand_range(-item_scatter, item_scatter), rand_range(-item_scatter, item_scatter))
 	add_child(item_sprite)
+
+
+func _upgrade():
+	depot_level += 1
+	if depot_level >= 10:
+		# make it clear
+		pass
+
+
+func upgradable():
+	if depot_level < 10:
+		if items.get("wood") and items.get("rock"):
+			return items.get("wood") >= 1 and items.get("rock") >= 2
