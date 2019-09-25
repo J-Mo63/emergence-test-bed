6cb4ec39ed2065e50074c36672d31ee6a9f7b185
Jonathan Moallem <jdmoallem@gmail.com>
2019-09-25 15:23:47 +1000

    Made main scene randomly generate food in spots across the game world

--- scripts/food_generator.gd
+++ scripts/food_generator.gd
@@ -1,20 +1,19 @@
-extends Node2D
+extends Timer
 
-onready var food_timer = $FoodTimer
 onready var food_scene = load("res://scenes/entities/food.tscn")
 
 
 func _ready():
-	food_timer.connect("timeout", self, "_generate_food")
-
-
-func _generate_food():
-	var food  = food_scene.instance()
-	food.position = get_random_screen_location()
-	add_child(food)
+	self.connect("timeout", self, "_generate_food")
 
 
 func get_random_screen_location():
 	var rand_x = rand_range(0, get_viewport().size.x + 1)
 	var rand_y = rand_range(0, get_viewport().size.y + 1)
 	return Vector2(rand_x, rand_y)
+
+
+func _generate_food():
+	var food  = food_scene.instance()
+	food.position = get_random_screen_location()
+	get_parent().add_child(food)
