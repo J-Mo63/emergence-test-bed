709346cc1ced67c696eee57602b526125070b754
Jonathan Moallem <jdmoallem@gmail.com>
2019-10-22 12:03:43 +1100

    Made camera pan and zoom with input

--- scenes/Camera2D.gd
+++ scenes/Camera2D.gd
@@ -1,1 +1,8 @@
 extends Camera2D
+
+func _unhandled_input(event):
+	print(event)
+	if event is InputEventPanGesture:
+		position = position + (event.delta * 30)
+	if event is InputEventMagnifyGesture:
+		zoom = zoom / event.factor
