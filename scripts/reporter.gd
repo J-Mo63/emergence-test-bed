414dcf51a6f92ced8aa373633fe3ab92905a5910
Jonathan Moallem <jdmoallem@gmail.com>
2019-10-22 18:23:34 +1100

    Made reporter script clear data on initialisation

--- scripts/reporter.gd
+++ scripts/reporter.gd
@@ -1,12 +1,19 @@
 class_name Reporter
 
+var filename = "user://gameplay_report.csv"
+
+func _init():
+	var file = File.new()
+	file.open(filename, File.WRITE)
+	file.close()
+
 func report_event(event_details):
 	var line = PoolStringArray([1, "event", event_details])
 	append_to_file(line)
 
 func append_to_file(line):
 	var file = File.new()
-	file.open("user://gameplay_report.csv", File.READ_WRITE)
+	file.open(filename, File.READ_WRITE)
 	file.seek_end()
 	file.store_csv_line(line)
 	file.close()
