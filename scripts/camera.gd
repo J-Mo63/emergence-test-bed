692a51f095d2a0e9648c8704f634df347ddd5a62
Jonathan Moallem <jdmoallem@gmail.com>
2019-10-22 14:40:05 +1100

    Added point-zooming to camera

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,8 +1,14 @@
 extends Camera2D
 
 func _unhandled_input(event):
-	print(event)
 	if event is InputEventPanGesture:
 		position = position + (event.delta * 30)
 	if event is InputEventMagnifyGesture:
-		zoom = zoom / event.factor
+		zoom_at_point(1/event.factor, event.position)
+
+func zoom_at_point(zoom_change, point):
+	var new_zoom = zoom * zoom_change
+	var new_pos = global_position + (-0.5 * get_viewport().size + point)*(zoom - new_zoom)
+	if new_zoom.x < 5 and new_zoom.x > 0.1:
+		zoom = new_zoom
+		global_position = new_pos
