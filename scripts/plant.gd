5ed2c999b50fac5477903765b3850f2f01933b86
Jonathan Moallem <jdmoallem@gmail.com>
2019-10-23 13:59:27 +1100

    Added plant scene and script

---
+++ scripts/plant.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+func _ready():
+	$Timer.connect("timeout", self, "grow")
+
+func grow():
+	var food = preload("res://scenes/entities/food.tscn").instance()
+	food.position = position
+	get_parent().add_child(food)
+	queue_free()
