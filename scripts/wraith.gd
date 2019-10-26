5848ca52f900cb9059832a094b5510454db3a99a
Jonathan Moallem <jdmoallem@gmail.com>
2019-10-26 21:48:58 +1100

    Made bored wraiths wander

--- scripts/wraith.gd
+++ scripts/wraith.gd
@@ -4,7 +4,6 @@ extends KinematicBody2D
 export (int) var speed = 80
 
 # Onready fields
-onready var target = self
 onready var target_position = position
 onready var anim_player = $AnimationPlayer
 onready var wraith_range = $Range
@@ -21,14 +20,19 @@ func _physics_process(_delta):
 func run_actions():
 	var dudes = get_tree().get_nodes_in_group("active_dude")
 	if dudes.empty():
-		print("Wraith is sad and lonely")
+		wander()
 	else:
-		set_target(get_closest(dudes))
+		target_position = get_closest(dudes).global_position
 		var bodies = wraith_range.get_overlapping_bodies()
 		for body in bodies:
 			if body.is_in_group("active_dude"):
 				body.lifespan = 0
 
+
+func wander():
+	if not target_position or position == target_position:
+		var new_dir = Vector2(rand_range(-300, 300), rand_range(-300, 300))
+		target_position = global_position + new_dir
 
 func get_closest(values):
 	var closest_value = values[0]
@@ -38,11 +42,6 @@ func get_closest(values):
 		if potential_len < current_len:
 			closest_value = value
 	return closest_value
-
-
-func set_target(value):
-	target = value
-	target_position = target.global_position
 
 
 func move():
